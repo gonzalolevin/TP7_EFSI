dfcfcd1562630fb4e354dde7a20262741b3a2a9f
uristrauss <uriel.strauss@gmail.com>
2023-07-31 15:58:50 -0300

    Seguimos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import Listado from './Listado';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout.js";
+import Estadisticas from "./Estadisticas.js";
+import Contactos from "./Contactos.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Listado/>
-    </div>
-  );
-}
+const App = () => {
+ return (
+   <>
+   
+     <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route path="/Contactos" element={<Contactos />}></Route>
+          <Route path="/Estadistcas" element={<Estadisticas/>}></Route>
+       </Routes>
+     </BrowserRouter>
+   </>
+ );
+};
 
 export default App;
--- src/Listado.js
+++ src/Listado.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import Personas from './Personas.js';
 
 function Listado()
